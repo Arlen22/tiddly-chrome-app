70c67ca88f258214219b2423edcfc59b342d63aa
Arlen22 <arlenbee@gmail.com>
2015-07-20 16:01:07 -0400

    Update with current version I have

--- src/window.js
+++ src/window.js
@@ -12,72 +12,107 @@
 //
 
 (function(){
+    /*window.getFile = function loadFile(cb){
+        chrome.fileSystem.chooseEntry({
+            type: 'openWritableFile', 
+            accepts:[{
+                extensions: ['html']
+            }] 
+        }, 
+        cb);
+         
+    };
+    return;*/
     var currentFileEntry;
     var webview;
+    var expectingClose = false;
     window.onload = function(event){
-        console.log(event);
-        document.getElementById('reload').onclick = loadWebview;
-        document.getElementById('load').onclick = loadFile;
-        if(window.tiddlyChromeAutoOpen){
-
-        }
-        webview = document.getElementById('test');
-        webview.addEventListener('contentload', function() {
-            webview.contentWindow.postMessage({ message: 'welcome-tiddly-chrome-file-saver' }, window.location.origin);
-        });
-        webview.addEventListener('newwindow', function(e) {
-          e.preventDefault();
-          // e.targetUrl contains the target URL of the original link click
-          // or window.open() call: use it to open your own window to it.
-          // Something to keep in mind: window.open() called from the
-          // app's event page is currently (Nov 2013) handicapped and buggy
-          // (e.g. it doesn't have access to local storage, including cookie
-          // store). You can try to use it here and below, but be prepare that
-          // it may sometimes produce bad results.
-          //chrome.app.window.create(e.targetUrl);
-          chrome.browser.openTab(e.targetUrl);
-        });
-        webview.addEventListener('consolemessage', function(e){
-            switch(e.level){
-                case -1: console.debug('Webview:', e.message); break;
-                case 0: console.log('Webview:', e.message); break;
-                case 1: console.warn('Webview:', e.message); break;
-                case 2: console.error('Webview:', e.message); break;
-                default: break;
+        chrome.runtime.getBackgroundPage(function(backgroundPage){
+            
+            console.log(event);
+            document.getElementById('reload').onclick = loadWebview;
+            document.getElementById('load').onclick = loadFile;
+            document.getElementById("close-window-button").onclick = function() {
+                expectingClose = true;
+                webview.contentWindow.postMessage({ message: 'onbeforeunload-tiddly-chrome-file-saver' }, window.location.origin);
+                //webview.executeScript({ code: 'window.postMessage({ message: "onbeforeunload", data: !!window.onbeforeunload ? window.onbeforeunload() : false }); ' }, function(res){
+                    //window.close();
+                //});
             }
-        });
-        webview.addEventListener('dialog', showPopup);
-        webview.addEventListener('exit', function(e) {
-          
-            webview.src = 'data:text/plain,Goodbye, world! reason:' + e.reason;
-          
-        });
-        webview.addEventListener('close', function() {
-            webview.src = 'about:blank';
-        });
-        webview.addEventListener('permissionrequest', function(e) {
-          switch (e.permission) {
-            case "download": e.request.allow(); break;
-            case "fullscreen": e.request.allow(); break;
-          }
-          console.log(e);
-        });
-        webview.style.webkitTransition = 'opacity 250ms';
-        webview.addEventListener('unresponsive', function() {
-            webview.style.opacity = '0.5';
-        });
-        webview.addEventListener('responsive', function() {
-            webview.style.opacity = '1';
-        });
-        webview.addEventListener('loadcommit', function(e) {
-            document.getElementById('hashQuery').value = webview.src.substr(webview.src.indexOf('#') + 1);;
-        });
-        if(window.tiddlyChromeAutoOpen) {
-            currentFileEntry = window.tiddlyChromeAutoOpen;
-            loadWebview();
-        } else { 
-            loadFile(); 
-        }
+            if(window.tiddlyChromeAutoOpen){
+
+            }
+            webview = document.getElementById('webview');
+            webview.addEventListener('contentload', function() {
+                if(webview.src == "about:blank") return;
+                
+                webview.executeScript({ file: 'emergencySaver.js' }, function( results ){
+                    webview.contentWindow.postMessage({ message: 'welcome-tiddly-chrome-file-saver' }, window.location.origin);
+                });
+                
+                //TODO: show a popup warning that the saver isn't activated yet. If the TW doesn't have the saver,
+                //      the user should not use this chrome app for editting TW, as it does not ask before closing.
+                //      We could inject a script to listen to wiki messages, but that's better done from inside TW.
+            });
+            webview.addEventListener('newwindow', function(e) {
+                e.preventDefault();
+                // e.targetUrl contains the target URL of the original link click
+                // or window.open() call: use it to open your own window to it.
+                // Something to keep in mind: window.open() called from the
+                // app's event page is currently (Nov 2013) handicapped and buggy
+                // (e.g. it doesn't have access to local storage, including cookie
+                // store). You can try to use it here and below, but be prepare that
+                // it may sometimes produce bad results.
+                //chrome.app.window.create(e.targetUrl);
+                chrome.browser.openTab(e.targetUrl);
+            });
+            webview.addEventListener('consolemessage', function(e){
+                switch(e.level){
+                    case -1: backgroundPage.console.debug('Webview:', e.message); break;
+                    case 0:  backgroundPage.console.log('Webview:', e.message); break;
+                    case 1:  backgroundPage.console.warn('Webview:', e.message); break;
+                    case 2:  backgroundPage.console.error('Webview:', e.message); break;
+                    default: break;
+                }
+            });
+            webview.addEventListener('dialog', showPopup);
+            webview.addEventListener('exit', function(e) {
+              
+                webview.src = 'data:text/plain,Goodbye, world! reason:' + e.reason;
+              
+            });
+            webview.addEventListener('close', function() {
+               
+                webview.src = 'about:blank';
+                
+                
+            });
+            webview.addEventListener('permissionrequest', function(e) {
+              switch (e.permission) {
+                case "download": e.request.allow(); break;
+                case "fullscreen": e.request.allow(); break;
+              }
+              console.log(e);
+            });
+            webview.style.webkitTransition = 'opacity 250ms';
+            webview.addEventListener('unresponsive', function() {
+                webview.style.opacity = '0.5';
+            });
+            webview.addEventListener('responsive', function() {
+                webview.style.opacity = '1';
+            });
+            webview.addEventListener('loadcommit', function(e) {
+                document.getElementById('hashQuery').value = webview.src.indexOf('#') > -1 ? webview.src.substr(webview.src.indexOf('#') + 1) : '';
+            });
+            if(window.tiddlyChromeAutoOpen) {
+                currentFileEntry = window.tiddlyChromeAutoOpen;
+                loadWebview();
+            } else { 
+                loadFile(); 
+            }
+            webview.canGoBack = false;
+            webview.canGoForward = false;
+        })
     };
     function loadWebview(){
         var hashQuery = document.getElementById('hashQuery').value;
@@ -123,14 +158,17 @@
         }
         function clicker(clickEvent){
             console.log(clickEvent);
-            
+            var endPopup = function(){
+                for(var i = 0; i < lightbox.length; i++){ lightbox[i].style.display = "none"; }
+                chrome.app.window.current().clearAttention();
+            };
             if(clickEvent.target.id == "popupCancel" || clickEvent.target.id == "popupNo"){
                 e.dialog.cancel();
-                for(var i = 0; i < lightbox.length; i++){ lightbox[i].style.display = "none"; }
+                endPopup();
             } else if (clickEvent.target.id == "popupOK" || clickEvent.target.id == "popupYes"){
                 e.dialog.ok(e.messageType == "prompt" ? document.getElementById('popupText').value : null);
-                for(var i = 0; i < lightbox.length; i++){ lightbox[i].style.display = "none"; }
-            }
+                endPopup();
+            } 
         };
         ['popupYes', 'popupNo', 'popupOK', 'popupCancel'].forEach(function(e,i){
             items[e] = document.getElementById(e);
@@ -145,7 +183,8 @@
         } else if(e.messageType === 'prompt'){
             setDisplay(['popupOK','popupCancel']);
         }
-        
+        chrome.app.window.current().drawAttention();
+        document.getElementById('popupText').value = e.defaultPromptText;
         document.getElementById('popupText').style.display = e.messageType === 'prompt' ? 'inline-block' : 'none';
         document.getElementById('popupLabel').innerText = e.messageText;
         
@@ -167,8 +206,15 @@
                 
             });
         }
+        if(event.data.message === "onbeforeunload-tiddly-chrome-file-saver" && expectingClose){
+            if(typeof(event.data.data) === "string"){
+                //show confirm box and close if true.
+            }
+        }
     });
     
-    
+    window.alert = function(text){
+        showPopup({messageType: 'alert', messageText: text, dialog : { cancel : function(){}, ok: console.log.bind(console) }, preventDefault: function(){} });
+    }
 
 })();
